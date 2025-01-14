5cc7f6b1865956712d76fd82f8d96b056596d97e
gunnhd <62600070+gunnhd@users.noreply.github.com>
2025-01-14 18:51:31 +0100

    Update Oppg6.py

--- Oppg6.py
+++ Oppg6.py
@@ -7,8 +7,7 @@ Created on Sun Jan 12 13:32:47 2025
 """
 """
 
-Oppg 6) Skriv en kode som plotter funksjonen 𝑓(𝑥) = −𝑥2
-− 5, for x på intervallet [-10,10].
+Oppg 6) Skriv en kode som plotter funksjonen 𝑓(𝑥) = −𝑥2− 5, for x på intervallet [-10,10].
 Hint: np.linspace(-10, 10, 200) gir en array med 200 punkter jevnt fordelt på intervallet
 [-10,10].
 """
